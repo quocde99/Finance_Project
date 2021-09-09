f41e85a54593c0447b77d21af3d7046b4a8c1f87
matornat2812 <18521190@gm.uit.edu.vn>
2021-09-09 14:17:39 +0700

    CODE GENERATOR DATA
    I have  check and fix, please check for me
    
    Co-Authored-By: ThucHH <88424781+thuchh@users.noreply.github.com>

--- resources/rawData/Object/jobGroup.py
+++
@@ -1,9 +0,0 @@
-from faker import Faker
-fake = Faker()
-class Job:
-    def __init__(self, Job, jobTitle, income_basic):
-        self.jobId = Job
-        self.jobTitle = jobTitle
-        # giá trị lương cơ bản
-        self.income_basic = income_basic
-
